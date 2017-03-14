4bac77a9fec2e0be0bb172fcea1800e01bc87413
Jonas Heinisch <jonas@heinisch-web.de>
2017-03-14 14:03:01 +0100

    Remove duplicate listeners (#11)
    * tried to remove duplicate listeners...
    
    * Solved multiple message problem :)

--- src/main/java/de/adesso/anki/AnkiConnector.java
+++ src/main/java/de/adesso/anki/AnkiConnector.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
@@ -20,22 +22,32 @@ import de.adesso.anki.messages.Message;
 public class AnkiConnector {
   
   private Socket socket;
+  private final String host;
+  private final int port;
   
   private PrintWriter writer;
   private NotificationReader reader;
   
-  private Multimap<Vehicle, MessageListener> listeners;
+  private Multimap<Vehicle, MessageListener> messageListeners;
+  private Map<Vehicle, NotificationListener> notificationListeners;
   
   public AnkiConnector(String host, int port) throws IOException {
+	this.host = host;
+	this.port = port;
     socket = new Socket(host, port);
     writer = new PrintWriter(socket.getOutputStream(), true);
     reader = new NotificationReader(socket.getInputStream());
     
-    listeners = ArrayListMultimap.create();
+    messageListeners = ArrayListMultimap.create();
+    notificationListeners = new HashMap<Vehicle, NotificationListener>();
   }
   
   public AnkiConnector(String host) throws IOException {
     this(host, 5000);
+  }
+  
+  public AnkiConnector(AnkiConnector anki) throws IOException{
+	  this(anki.host, anki.port);
   }
   
   public synchronized List<Vehicle> findVehicles() {
@@ -69,13 +81,19 @@ public class AnkiConnector {
       throw new RuntimeException("connect failed");
     }
     
-    reader.addListener((line) -> {
-      if (line.startsWith(vehicle.getAddress())) {
-        String messageString = line.replaceFirst(vehicle.getAddress()+";", "");
-        Message message = Message.parse(messageString);
-        fireMessageReceived(vehicle, message);
-      }
-    });
+    NotificationListener carsNotificationListener = (line) -> {
+        if (line.startsWith(vehicle.getAddress())) {
+          String messageString = line.replaceFirst(vehicle.getAddress()+";", "");
+          Message message = Message.parse(messageString);
+          fireMessageReceived(vehicle, message);
+        }
+      };
+    // check if there is a notification listener -> if yes, remove it! (otherwise it will be a listener we cannot track anymore...)
+    if(notificationListeners.containsKey(vehicle)){
+    	reader.removeListener(notificationListeners.get(vehicle));
+    }
+    notificationListeners.put(vehicle, carsNotificationListener);
+    reader.addListener(carsNotificationListener);
   }
   
   public synchronized void sendMessage(Vehicle vehicle, Message message) {
@@ -84,16 +102,16 @@ public class AnkiConnector {
   }
   
   public void addMessageListener(Vehicle vehicle, MessageListener<? extends Message> listener) {
-    listeners.put(vehicle, listener);
+    messageListeners.put(vehicle, listener);
   }
   
   public void removeMessageListener(Vehicle vehicle, MessageListener<? extends Message> listener) {
-    listeners.remove(vehicle, listener);
+    messageListeners.remove(vehicle, listener);
   }
   
   @SuppressWarnings("unchecked")
   public void fireMessageReceived(Vehicle vehicle, Message message) {
-    for (MessageListener l : listeners.get(vehicle)) {
+    for (MessageListener l : messageListeners.get(vehicle)) {
       l.messageReceived(message);
     }
   }
@@ -101,6 +119,7 @@ public class AnkiConnector {
   public synchronized void disconnect(Vehicle vehicle) {
     writer.println("DISCONNECT;"+vehicle.getAddress());
     reader.waitFor("DISCONNECT;");
+    reader.removeListener(notificationListeners.remove(vehicle));
   }
 
   public void close() {
